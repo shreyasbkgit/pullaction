993211e14b2111b190d2d5ea0741c4f086b60d28
shreyasbkgit <shreyasbhatk.scs24@bmsce.ac.in>
2025-07-11 18:18:43 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
 # A friendly Python program to greet you!
 def greet_user():
     print("Hello! Hope you're having a fantastic day! 😊")
-    print("Hello! Hope you're having a fantastic day! 😊")
 greet_user()
